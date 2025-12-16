fe7b8b173453a429116ca2ee048b879cfd2be69a
Vvv27v <vithues@hotmail.fr>
2025-12-16 15:27:41 +0100

    add footer (.php and .css)

---
+++ app/Views/footer.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * View : HTML footer
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>KeepMyPet - Accueil</title>
+  <link rel="stylesheet" href="../../../public/assets/css/footer.css">
+</head>
+
+<body>
+
+  <footer>
+    <div class="logo-container">
+      <img src="../../../public/assets/images/G2Cidees_Logo.png" alt="G2Cidées Logo">
+    </div>
+
+    <div class="keepmypet-container">
+      <h3>KeepMyPet</h3>
+      <p>A propos de KeepMyPet</p>
+      <p>Contact</p>
+    </div>
+
+    <div class="g2cidees-container">
+      <h3>G2Cidées</h3>
+      <p>A propos de G2Cidées</p>
+      <p>Contact</p>
+    </div>
+
+    <div class="copyright-container">
+      <h3>Copyright</h3>
+      <p>©G2Cidées 2025</p>
+    </div>
+  </footer>
+</body>
+
+</html>
